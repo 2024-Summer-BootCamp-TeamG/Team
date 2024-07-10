7d3eb3afa9946df2ad9492debd615b06a3d5ebc6
hoya04 <164477482+hoya04@users.noreply.github.com>
2024-07-11 02:54:02 +0900

    bug fix

--- backend/albums/urls.py
+++ backend/albums/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.views.decorators.csrf import csrf_exempt
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -18,7 +19,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('analyze-image/', AnalyzeImageView.as_view(), name='analyze_image'),
 # Swagger UI
 path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+path('api/analyze-image/', csrf_exempt(AnalyzeImageView.as_view()), name='analyze_image'),
 ]
