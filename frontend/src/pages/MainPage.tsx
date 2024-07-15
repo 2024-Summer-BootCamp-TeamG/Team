084c1f34b85497cfc1bbb69fc6ad0197185dcaa9
(choiseyeon) <(csycsi1002@naver.com)>
2024-07-16 02:44:28 +0900

    텍스트 생성 페이지

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -52,13 +52,13 @@ export default function MainPage({ children }: MainPageProps) {
       
         {children}
 
-        <div className="absolute items-center justify-center left-1/2">
+        <div className="absolute items-center justify-center left-1/2 top-1/4">
           <div className="relative text-teal-200 text-8xl">HOW ARE YOU <br />FEELING TODAY?</div>
           <div className="relative text-5xl text-teal-200 mt-4">오늘 당신의 기분과 음악을 알려드릴게요!</div>
       
         </div>
       
-        <div className="relative text-center flex items-center justify-center left-1/3 top-2/5">
+        <div className="relative text-center flex items-center justify-center left-1/3 top-[38rem] left-80">
           <div className="flex items-center justify-center w-[14rem] h-[3rem] text-xl bg-gray-800 border border-white border text-white rounded-3xl mr-9">로그인</div>
           <div className="flex items-center justify-center w-[14rem] h-[3rem] text-xl bg-slate-300 text-black items-center border border-white rounded-3xl ml-9">회원가입</div>
         </div>
