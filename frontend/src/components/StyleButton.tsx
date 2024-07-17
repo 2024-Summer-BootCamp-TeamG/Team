88367b1e044eb262c7df9324b936dff42523627b
GirlsLikeMyY <psr2560@naver.com>
2024-07-17 17:06:12 +0900

    스타일 선택 페이지 toggle처럼 하나만 선택되게 구현

--- frontend/src/components/StyleButton.tsx
+++ frontend/src/components/StyleButton.tsx
@@ -1,39 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 interface StyleButtonProps {
   buttonText?: string;
   className?: string;
   onClick?: () => void;
+  isSelected?: boolean; // 선택 상태를 전달받을 prop 추가
+  onToggle?: () => void; // 토글 이벤트 핸들러 추가
 }
 
 function StyleButton({
   buttonText = 'example',
   className = '',
   onClick,
+  isSelected = false,
+  onToggle,
 }: StyleButtonProps) {
-  const [isClicked, setIsClicked] = useState(false); // 클릭 상태를 관리하는 상태
-
   const handleClick = () => {
-    setIsClicked(true); // 클릭 시 클릭 상태 변경
-    if (onClick) {
-      onClick(); // 클릭 이벤트 핸들러 실행
+    if (onToggle) {
+      onToggle(); // 토글 이벤트 핸들러 호출
     }
   };
 
   return (
-    <>
-      <div
-        className={`relative z-10 flex justify-center self-center rounded-[60px] border-2 border-white bg-gradient-to-b from-white/20 to-slate-400/10 shadow backdrop-blur-xl sm:mt-0 ${className} ${isClicked ? 'bg-white text-black' : ''}`}
+    <div
+      className={`relative z-10 flex justify-center self-center rounded-[60px] border-2 border-white bg-gradient-to-b from-white/20 to-slate-400/10 shadow backdrop-blur-xl sm:mt-0 ${className} ${isSelected ? 'bg-white text-black' : ''}`}
+    >
+      <button
+        type="button"
+        className={`button-custom-stroke w-full rounded-[60px] border py-2 text-center text-[1.5rem] ${isSelected ? 'text-black' : 'text-white'} hover:bg-white hover:text-black`}
+        onClick={handleClick}
       >
-        <button
-          type="button"
-          className={`button-custom-stroke w-full rounded-[60px] border py-2 text-center text-[1.5rem] ${isClicked ? 'text-black' : 'text-white'} hover:bg-white hover:text-black`}
-          onClick={handleClick}
-        >
-          {buttonText}
-        </button>
-      </div>
-    </>
+        {buttonText}
+      </button>
+    </div>
   );
 }
 
--- frontend/src/pages/SelectStylePage.tsx
+++ frontend/src/pages/SelectStylePage.tsx
@@ -7,11 +7,13 @@ import { Link } from 'react-router-dom';
 
 // 스타일 선택 페이지
 function SelectStylePage() {
-  const [selectedButton, setSelectedButton] = useState(''); // 선택된 버튼 상태
+  const [selectedButton, setSelectedButton] = useState<string>(''); // 선택된 버튼 상태
 
-  // 버튼 클릭 핸들러
-  const handleButtonClick = (buttonText: React.SetStateAction<string>) => {
-    setSelectedButton(buttonText); // 선택된 버튼으로 상태 업데이트
+  // 특정 버튼의 선택 상태 토글 함수
+  const toggleButton = (buttonText: string) => {
+    setSelectedButton((prevSelected) =>
+      prevSelected === buttonText ? '' : buttonText,
+    );
   };
 
   return (
@@ -24,56 +26,66 @@ function SelectStylePage() {
               <StyleButton
                 className={`h-[9rem] w-[21rem] ${selectedButton === '모던한' ? 'selected' : ''}`}
                 buttonText="모던한"
-                onClick={() => handleButtonClick('모던한')}
+                isSelected={selectedButton === '모던한'}
+                onToggle={() => toggleButton('모던한')}
               />
               <StyleButton
                 className={`h-[9rem] w-[21rem] ${selectedButton === '장난스러운' ? 'selected' : ''}`}
                 buttonText="장난스러운"
-                onClick={() => handleButtonClick('장난스러운')}
+                isSelected={selectedButton === '장난스러운'}
+                onToggle={() => toggleButton('장난스러운')}
               />
               <StyleButton
                 className={`h-[9rem] w-[21rem] ${selectedButton === '재미난' ? 'selected' : ''}`}
                 buttonText="재미난"
-                onClick={() => handleButtonClick('재미난')}
+                isSelected={selectedButton === '재미난'}
+                onToggle={() => toggleButton('재미난')}
               />
             </div>
             <div className="mx-auto flex w-auto flex-row justify-between space-x-4">
               <StyleButton
                 className={`h-[9rem] w-[21rem] ${selectedButton === '간단한' ? 'selected' : ''}`}
                 buttonText="간단한"
-                onClick={() => handleButtonClick('간단한')}
+                isSelected={selectedButton === '간단한'}
+                onToggle={() => toggleButton('간단한')}
               />
               <StyleButton
                 className={`h-[9rem] w-[21rem] ${selectedButton === '다이나믹한' ? 'selected' : ''}`}
                 buttonText="다이나믹한"
-                onClick={() => handleButtonClick('다이나믹한')}
+                isSelected={selectedButton === '다이나믹한'}
+                onToggle={() => toggleButton('다이나믹한')}
               />
               <StyleButton
                 className={`h-[9rem] w-[21rem] ${selectedButton === '공식적인' ? 'selected' : ''}`}
                 buttonText="공식적인"
-                onClick={() => handleButtonClick('공식적인')}
+                isSelected={selectedButton === '공식적인'}
+                onToggle={() => toggleButton('공식적인')}
               />
               <StyleButton
                 className={`h-[9rem] w-[21rem] ${selectedButton === '고급진' ? 'selected' : ''}`}
                 buttonText="고급진"
-                onClick={() => handleButtonClick('고급진')}
+                isSelected={selectedButton === '고급진'}
+                onToggle={() => toggleButton('고급진')}
               />
             </div>
             <div className="mx-auto flex w-3/4 flex-row justify-between space-x-12">
               <StyleButton
                 className={`h-[9rem] w-[21rem] ${selectedButton === '창의적인' ? 'selected' : ''}`}
                 buttonText="창의적인"
-                onClick={() => handleButtonClick('창의적인')}
+                isSelected={selectedButton === '창의적인'}
+                onToggle={() => toggleButton('창의적인')}
               />
               <StyleButton
                 className={`h-[9rem] w-[21rem] ${selectedButton === '힙한' ? 'selected' : ''}`}
                 buttonText="힙한"
-                onClick={() => handleButtonClick('힙한')}
+                isSelected={selectedButton === '힙한'}
+                onToggle={() => toggleButton('힙한')}
               />
               <StyleButton
                 className={`h-[9rem] w-[21rem] ${selectedButton === '귀여운' ? 'selected' : ''}`}
                 buttonText="귀여운"
-                onClick={() => handleButtonClick('귀여운')}
+                isSelected={selectedButton === '귀여운'}
+                onToggle={() => toggleButton('귀여운')}
               />
             </div>
             <div className="mt-56 flex w-full flex-row justify-between px-4">
