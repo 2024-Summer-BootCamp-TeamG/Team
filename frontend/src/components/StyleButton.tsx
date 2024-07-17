7c4d2b1aaf80ba40a75c43448e7c649b36fe4a62
GirlsLikeMyY <psr2560@naver.com>
2024-07-17 16:55:19 +0900

    스타일 선택 페이지 버튼 클릭 효과 구현

--- frontend/src/components/StyleButton.tsx
+++ frontend/src/components/StyleButton.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function StyleButton({ buttonText = 'example', className = '' }) {
+interface StyleButtonProps {
+  buttonText?: string;
+  className?: string;
+  onClick?: () => void;
+}
+
+function StyleButton({
+  buttonText = 'example',
+  className = '',
+  onClick,
+}: StyleButtonProps) {
+  const [isClicked, setIsClicked] = useState(false); // 클릭 상태를 관리하는 상태
+
+  const handleClick = () => {
+    setIsClicked(true); // 클릭 시 클릭 상태 변경
+    if (onClick) {
+      onClick(); // 클릭 이벤트 핸들러 실행
+    }
+  };
+
   return (
     <>
       <div
-        className={`relative z-10 flex justify-center self-center rounded-[60px] border-2 border-white bg-gradient-to-b from-white/20 to-slate-400/10 shadow backdrop-blur-xl sm:mt-0 ${className}`}
+        className={`relative z-10 flex justify-center self-center rounded-[60px] border-2 border-white bg-gradient-to-b from-white/20 to-slate-400/10 shadow backdrop-blur-xl sm:mt-0 ${className} ${isClicked ? 'bg-white text-black' : ''}`}
       >
         <button
           type="button"
-          className="button-custom-stroke w-full rounded-[60px] border py-2 text-center text-[1.5rem] text-white hover:bg-white hover:text-black"
+          className={`button-custom-stroke w-full rounded-[60px] border py-2 text-center text-[1.5rem] ${isClicked ? 'text-black' : 'text-white'} hover:bg-white hover:text-black`}
+          onClick={handleClick}
         >
           {buttonText}
         </button>
--- frontend/src/pages/SelectStylePage.tsx
+++ frontend/src/pages/SelectStylePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Background from '../components/Background.tsx';
 import NavBar from '../components/NavBar.tsx';
 import StyleButton from '../components/StyleButton.tsx';
@@ -7,6 +7,13 @@ import { Link } from 'react-router-dom';
 
 // 스타일 선택 페이지
 function SelectStylePage() {
+  const [selectedButton, setSelectedButton] = useState(''); // 선택된 버튼 상태
+
+  // 버튼 클릭 핸들러
+  const handleButtonClick = (buttonText: React.SetStateAction<string>) => {
+    setSelectedButton(buttonText); // 선택된 버튼으로 상태 업데이트
+  };
+
   return (
     <div className="relative flex h-screen w-screen flex-col items-center justify-center bg-[#000000] bg-cover">
       <div className="relative h-full w-full">
@@ -14,32 +21,60 @@ function SelectStylePage() {
           <NavBar />
           <div className="w-auto flex-col items-center justify-center space-y-8">
             <div className="mx-auto flex w-3/4 flex-row justify-between space-x-12">
-              <StyleButton className="h-[9rem] w-[21rem]" buttonText="모던한" />
               <StyleButton
-                className="h-[9rem] w-[21rem]"
-                buttonText="장난스러운"
+                className={`h-[9rem] w-[21rem] ${selectedButton === '모던한' ? 'selected' : ''}`}
+                buttonText="모던한"
+                onClick={() => handleButtonClick('모던한')}
               />
-              <StyleButton className="h-[9rem] w-[21rem]" buttonText="재미난" />
+              <StyleButton
+                className={`h-[9rem] w-[21rem] ${selectedButton === '장난스러운' ? 'selected' : ''}`}
+                buttonText="장난스러운"
+                onClick={() => handleButtonClick('장난스러운')}
+              />
+              <StyleButton
+                className={`h-[9rem] w-[21rem] ${selectedButton === '재미난' ? 'selected' : ''}`}
+                buttonText="재미난"
+                onClick={() => handleButtonClick('재미난')}
+              />
             </div>
             <div className="mx-auto flex w-auto flex-row justify-between space-x-4">
-              <StyleButton className="h-[9rem] w-[21rem]" buttonText="간단한" />
               <StyleButton
-                className="h-[9rem] w-[21rem]"
+                className={`h-[9rem] w-[21rem] ${selectedButton === '간단한' ? 'selected' : ''}`}
+                buttonText="간단한"
+                onClick={() => handleButtonClick('간단한')}
+              />
+              <StyleButton
+                className={`h-[9rem] w-[21rem] ${selectedButton === '다이나믹한' ? 'selected' : ''}`}
                 buttonText="다이나믹한"
+                onClick={() => handleButtonClick('다이나믹한')}
               />
               <StyleButton
-                className="h-[9rem] w-[21rem]"
+                className={`h-[9rem] w-[21rem] ${selectedButton === '공식적인' ? 'selected' : ''}`}
                 buttonText="공식적인"
+                onClick={() => handleButtonClick('공식적인')}
               />
-              <StyleButton className="h-[9rem] w-[21rem]" buttonText="고급진" />
+              <StyleButton
+                className={`h-[9rem] w-[21rem] ${selectedButton === '고급진' ? 'selected' : ''}`}
+                buttonText="고급진"
+                onClick={() => handleButtonClick('고급진')}
+              />
             </div>
             <div className="mx-auto flex w-3/4 flex-row justify-between space-x-12">
               <StyleButton
-                className="h-[9rem] w-[21rem]"
+                className={`h-[9rem] w-[21rem] ${selectedButton === '창의적인' ? 'selected' : ''}`}
                 buttonText="창의적인"
+                onClick={() => handleButtonClick('창의적인')}
               />
-              <StyleButton className="h-[9rem] w-[21rem]" buttonText="힙한" />
-              <StyleButton className="h-[9rem] w-[21rem]" buttonText="귀여운" />
+              <StyleButton
+                className={`h-[9rem] w-[21rem] ${selectedButton === '힙한' ? 'selected' : ''}`}
+                buttonText="힙한"
+                onClick={() => handleButtonClick('힙한')}
+              />
+              <StyleButton
+                className={`h-[9rem] w-[21rem] ${selectedButton === '귀여운' ? 'selected' : ''}`}
+                buttonText="귀여운"
+                onClick={() => handleButtonClick('귀여운')}
+              />
             </div>
             <div className="mt-56 flex w-full flex-row justify-between px-4">
               <Link to="/choosecolor">
