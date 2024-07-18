ab19e14880e17fe6854168712115136047e12543
Taeho KIM <laferrari963@naver.com>
2024-07-18 17:23:50 +0900

    lists 중간저장(수정필요)

--- backend/contents/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import SavedContent
-
-class SavedContentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SavedContent
-        fields = ['id', 'user', 'logo_url', 'poster_url', 'audio_url', 'created_at']
-
