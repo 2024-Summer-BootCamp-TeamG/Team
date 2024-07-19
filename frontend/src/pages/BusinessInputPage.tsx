1513d605f7d5eaef622d216a4aa65647be077a19
Lee chen woo <oop20811@tukorea.ac.kr>
2024-07-19 21:08:32 +0900

    비즈니스퍼블리싱 수정

--- frontend/src/pages/BusinessInputPage.tsx
+++ frontend/src/pages/BusinessInputPage.tsx
@@ -4,28 +4,25 @@ import Background from '../components/Background';
 import NavBar from '../components/NavBar';
 import { businessInputState } from '../recoil/BusinessInputAtom';
 import { Link } from 'react-router-dom';
+import MoveButton from '../components/MoveButton';
 
 function BusinessInputPage() {
   const [businessInput, setBusinessInput] = useRecoilState(businessInputState);
-  const [isButtonClicked, setIsButtonClicked] = useState(false); // 버튼 클릭 상태를 관리하는 상태를 추가합니다.
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
   const handleSubmit = (event: FormEvent) => {
-    event.preventDefault(); // 폼 제출의 기본 동작을 막습니다.
+    event.preventDefault();
     console.log('입력된 텍스트:', businessInput);
-    setIsButtonClicked(true); // 버튼이 클릭되었음을 표시합니다.
-    // 여기에 다음 버튼을 클릭했을 때 실행할 코드를 추가하세요.
-    // 예를 들어, 다음 페이지로 이동하거나, 서버에 데이터를 전송하는 등의 작업을 수행할 수 있습니다.
+    setIsButtonClicked(true);
   };
 
   return (
     <div className="relative flex h-screen w-screen flex-col items-center justify-center bg-cover">
       <Background>
         <NavBar />
-        <div className="relative h-[67.5rem] w-[120rem]">
+        <div className="absolute h-[67.5rem] w-[120rem]">
           <div className="flex h-full w-full items-start justify-center p-4">
             <form onSubmit={handleSubmit}>
-              {' '}
-              {/* 폼 제출 시 handleSubmit 함수가 호출됩니다. */}
               <div className="left-[27rem] top-[27rem] h-[12.88rem] w-[68.63rem]">
                 <textarea
                   className="absolute left-[27rem] top-[27rem] h-[12.88rem] w-[68.63rem] resize-none overflow-auto rounded-[2.5rem] border-2 bg-white bg-gradient-to-b from-white/20 to-slate-400/10 p-2 text-3xl placeholder-violet-600 opacity-50 shadow outline-none backdrop-blur-xl"
@@ -35,9 +32,8 @@ function BusinessInputPage() {
                 />
               </div>
               <Link to="/texi">
-                <input
-                  type="submit"
-                  value="다음"
+                <MoveButton
+                  buttonText="다음"
                   className={`font-['Cafe24 Danjunghae'] absolute left-[83.1rem] top-[46.3rem] h-[5rem] w-[12.5rem] rounded-[2.5rem] bg-white/50 text-center text-3xl font-normal focus:outline-none ${isButtonClicked ? 'text-blue-800' : 'text-white'}`}
                 />
               </Link>
@@ -49,9 +45,10 @@ function BusinessInputPage() {
             </p>
           </div>
 
-          <button className="font-['Cafe24 Danjunghae'] absolute left-[27rem] top-[46.3rem] h-[5rem] w-[12.5rem] rounded-[2.5rem] bg-white/50 text-center text-3xl font-normal text-white">
-            이전
-          </button>
+          <MoveButton
+            buttonText="이전"
+            className="font-['Cafe24 Danjunghae'] absolute left-[27rem] top-[46.3rem] h-[5rem] w-[12.5rem] rounded-[2.5rem] bg-white/50 text-center text-3xl font-normal text-white"
+          />
         </div>
       </Background>
     </div>
--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -3,9 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Background from '../components/Background';
 import NavBar from '../components/NavBar';
-import { useNavigate } from 'react-router-dom';
 
-// Input 컴포넌트 정의
 const Input: React.FC<{
   type: string;
   placeholder: string;
@@ -42,7 +40,7 @@ const Button: React.FC<{
 function SignInPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate(); // useNavigate 훅 사용
+  const navigate = useNavigate();
 
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(e.target.value);
@@ -70,8 +68,7 @@ function SignInPage() {
 
       if (response.status === 200) {
         alert('로그인이 성공적으로 완료되었습니다.');
-        // 여기서 로그인 성공 후의 로직을 추가할 수 있습니다. 예를 들어, 리다이렉션 등.
-        navigate('/pictureupload'); // 로그인 성공 후 페이지 이동
+        navigate('/pictureupload');
       } else {
         alert('로그인에 실패했습니다.');
       }
@@ -93,10 +90,10 @@ function SignInPage() {
     <div className="relative flex h-screen w-screen flex-col items-center justify-center bg-cover">
       <Background>
         <NavBar />
-        <div className="h-[67.5rem] w-[120rem]">
-          <div className="absolute left-[20rem] top-[7.19rem] h-[53.13rem] w-[50rem] rounded-[2.5rem] border-2 border-white bg-white/30 opacity-60 shadow backdrop-blur-[3.44rem]" />
+        <div className="relative h-[67.5rem] w-[120rem]">
+          <div className="absolute left-[35rem] top-[7.19rem] h-[53.13rem] w-[50rem] rounded-[2.5rem] border-2 border-white bg-white/30 opacity-60 shadow backdrop-blur-[3.44rem]" />
           <form onSubmit={handleSubmit}>
-            <div className="bg-white/opacity-20 absolute left-[34.5rem] top-[38rem] h-[3.75rem] w-[25rem] rounded-[1.25rem] border-2 border-white">
+            <div className="bg-white/opacity-20 absolute left-[47.5rem] top-[38rem] h-[3.75rem] w-[25rem] rounded-[1.25rem] border-2 border-white">
               <Input
                 type="password"
                 placeholder="비밀번호"
@@ -104,7 +101,7 @@ function SignInPage() {
                 onChange={handlePasswordChange}
               />
             </div>
-            <div className="bg-white/opacity-20 absolute left-[34.5rem] top-[32rem] h-[3.75rem] w-[25rem] rounded-[1.25rem] border-2 border-white">
+            <div className="bg-white/opacity-20 absolute left-[47.5rem] top-[32rem] h-[3.75rem] w-[25rem] rounded-[1.25rem] border-2 border-white">
               <Input
                 type="text"
                 placeholder="아이디"
@@ -112,10 +109,10 @@ function SignInPage() {
                 onChange={handleUsernameChange}
               />
             </div>
-            <div className="font-['Cafe24 Danjunghae'] absolute left-[38rem] top-[15.25rem] text-center text-[6rem] font-normal text-white">
+            <div className="font-['Cafe24 Danjunghae'] absolute left-[52rem] top-[15.25rem] text-center text-[6rem] font-normal text-white">
               로그인
             </div>
-            <div className="font-['Cafe24 Danjunghae'] absolute left-[34.5rem] top-[52.5rem] flex w-[25rem] items-center justify-center rounded-[1.25rem] text-xl font-normal text-black">
+            <div className="font-['Cafe24 Danjunghae'] absolute left-[47.5rem] top-[52.5rem] flex w-[25rem] items-center justify-center rounded-[1.25rem] text-xl font-normal text-black">
               <Button type="submit" label="로그인하기" onClick={handleSubmit} />
             </div>
           </form>
