a7038ff6b84fe83f7dd02c7fd2f13ed74f7db269
(choiseyeon) <(csycsi1002@naver.com)>
2024-07-24 16:40:22 +0900

    스타일 선택페이지

--- frontend/src/pages/ChooseStylePage.tsx
+++ frontend/src/pages/ChooseStylePage.tsx
@@ -14,7 +14,23 @@ function ChooseStyle() {
         <div className="mt-28 flex flex-col items-center justify-center gap-16">
           <div className="relative h-[0rem] w-[70rem] border-2 border-white">
             <button
-              className="absolute -top-24 left-96 -translate-x-1/2 transform"
+              className="absolute -top-28 left-96 -translate-x-1/2 transform"
+              style={{
+                height: '8rem',
+                width: '4rem',
+                border: 'none',
+                background: 'transparent',
+              }}
+              onClick={() => alert('Button Clicked')}
+            >
+              <img
+                src={Vector}
+                alt="Vector Icon"
+                style={{ height: '100%', width: '100%' }}
+              />
+            </button>
+            <button
+              className="absolute -top-10 left-32 -translate-x-1/2 transform"
               style={{
                 height: '8rem',
                 width: '4rem',
@@ -47,28 +63,8 @@ function ChooseStyle() {
                 style={{ height: '100%', width: '100%' }}
               />
             </button>
-          </div>
-          <div className="relative h-[0rem] w-[70rem] border-2 border-white">
             <button
-              className="absolute -top-28 left-1/2 -translate-x-1/2 transform"
-              style={{
-                height: '8rem',
-                width: '4rem',
-                border: 'none',
-                background: 'transparent',
-              }}
-              onClick={() => alert('Button Clicked')}
-            >
-              <img
-                src={Vector}
-                alt="Vector Icon"
-                style={{ height: '100%', width: '100%' }}
-              />
-            </button>
-          </div>
-          <div className="relative h-[0rem] w-[70rem] border-2 border-white">
-            <button
-              className="absolute -top-16 left-56 -translate-x-1/2 transform"
+              className="absolute -top-16 left-60 -translate-x-1/2 transform"
               style={{
                 height: '8rem',
                 width: '4rem',
@@ -87,6 +83,90 @@ function ChooseStyle() {
           <div className="relative h-[0rem] w-[70rem] border-2 border-white">
             <button
               className="absolute -top-28 left-1/2 -translate-x-1/2 transform"
+              style={{
+                height: '8rem',
+                width: '4rem',
+                border: 'none',
+                background: 'transparent',
+              }}
+              onClick={() => alert('Button Clicked')}
+            >
+              <img
+                src={Vector}
+                alt="Vector Icon"
+                style={{ height: '100%', width: '100%' }}
+              />
+            </button>
+            <button
+              className="absolute -top-16 left-60 -translate-x-1/2 transform"
+              style={{
+                height: '8rem',
+                width: '4rem',
+                border: 'none',
+                background: 'transparent',
+              }}
+              onClick={() => alert('Button Clicked')}
+            >
+              <img
+                src={Vector}
+                alt="Vector Icon"
+                style={{ height: '100%', width: '100%' }}
+              />
+            </button>
+          </div>
+          <div className="relative h-[0rem] w-[70rem] border-2 border-white">
+            <button
+              className="absolute -top-16 left-60 -translate-x-1/2 transform"
+              style={{
+                height: '8rem',
+                width: '4rem',
+                border: 'none',
+                background: 'transparent',
+              }}
+              onClick={() => alert('Button Clicked')}
+            >
+              <img
+                src={Vector}
+                alt="Vector Icon"
+                style={{ height: '100%', width: '100%' }}
+              />
+            </button>
+            <button
+              className="absolute -top-16 left-60 -translate-x-1/2 transform"
+              style={{
+                height: '8rem',
+                width: '4rem',
+                border: 'none',
+                background: 'transparent',
+              }}
+              onClick={() => alert('Button Clicked')}
+            >
+              <img
+                src={Vector}
+                alt="Vector Icon"
+                style={{ height: '100%', width: '100%' }}
+              />
+            </button>
+          </div>
+          <div className="relative h-[0rem] w-[70rem] border-2 border-white">
+            <button
+              className="absolute -top-28 left-1/2 -translate-x-1/2 transform"
+              style={{
+                height: '8rem',
+                width: '4rem',
+                border: 'none',
+                background: 'transparent',
+              }}
+              onClick={() => alert('Button Clicked')}
+            >
+              <img
+                src={Vector}
+                alt="Vector Icon"
+                style={{ height: '100%', width: '100%' }}
+              />
+            </button>
+            <button
+              className="absolute -top-16 left-60 -translate-x-1/2 transform"
               style={{
                 height: '8rem',
                 width: '4rem',
