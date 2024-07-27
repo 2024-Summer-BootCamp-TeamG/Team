a5af7c1c685c3d69b699238e4097e45d2522dc05
Taeho KIM <laferrari963@naver.com>
2024-07-27 18:44:54 +0900

    chore: API 엔드포인트 수정

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -32,9 +32,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/',include('users.urls')),
-    path('prompts/',include('prompts.urls')),
-    path('promotions/',include('promotions.urls')),
+    path('api/users/',include('users.urls')),
+    path('api/prompts/',include('prompts.urls')),
+    path('api/promotions/',include('promotions.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
