43cb52caf15913d785a5bd4a9db7fd42fffd48dd
Lee chen woo <oop20811@tukorea.ac.kr>
2024-07-30 01:49:51 +0900

    메인페이지 수정

--- frontend/src/components/Background3.tsx
+++ frontend/src/components/Background3.tsx
@@ -1,4 +1,120 @@
-export default function Background({ children }: BackgroundProps) {
-  return (
-<div className="relative flex h-screen w-screen flex-col items-center justify-center bg-black bg-cover" style={{backgroundImage: `url(${YourBackgroundImageUrl})`}}>
+import { ReactNode, useEffect, useState } from 'react';
+import Circle from '../components/CustomCircle';
+import FullNote1 from '../assets/MusicNote/FullNote1.svg';
+import FullNote2 from '../assets/MusicNote/FullNote2.svg';
+import FullNote3 from '../assets/MusicNote/FullNote3.svg';
+import FullNote4 from '../assets/MusicNote/FullNote4.svg';
+import FullNote5 from '../assets/MusicNote/FullNote5.svg';
+import FullNote6 from '../assets/MusicNote/FullNote6.svg';
+import FullNote7 from '../assets/MusicNote/FullNote7.svg';
+import smallNote from '../assets/smallNote.png';
 
+interface BackgroundProps {
+  children: ReactNode;
+}
+
+interface CoordinateCircle {
+  size: string;
+  left?: string;
+  right?: string;
+  top: string;
+  backgroundColor: string;
+}
+
+interface CoordinateNote {
+  src: string;
+  left?: string;
+  right?: string;
+  top: string;
+}
+
+export default function Background({ children }: BackgroundProps) {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.pageYOffset);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const coordinates: CoordinateCircle[] = [
+    { size: '100px', left: '50%', top: '70%', backgroundColor: '#7fc8d6' },
+    { size: '150px', left: '40%', top: '50%', backgroundColor: '#5d3fd3' },
+    { size: '50px', left: '60%', top: '20%', backgroundColor: '#7fc8d6' },
+    { size: '200px', left: '70%', top: '50%', backgroundColor: '#9370db' },
+    { size: '75px', left: '40%', top: '15%', backgroundColor: '#9370db' },
+    { size: '125px', left: '50%', top: '30%', backgroundColor: '#9370db' },
+  ];
+  const notes: CoordinateNote[] = [
+    { src: FullNote1, right: '90%', top: '70%' },
+    { src: FullNote2, left: '10%', top: '80%' },
+    { src: FullNote3, left: '90%', top: '70%' },
+    { src: FullNote4, left: '25%', top: '90%' },
+    { src: FullNote5, left: '80%', top: '15%' },
+    { src: FullNote6, left: '10%', top: '30%' },
+    { src: FullNote7, left: '2%', top: '40%' },
+  ];
+
+  const smallnote: CoordinateNote[] = [
+    { src: smallNote, right: '90%', top: '70%' },
+    { src: smallNote, left: '50%', top: '80%' },
+    { src: smallNote, left: '90%', top: '45%' },
+    { src: smallNote, left: '25%', top: '90%' },
+    { src: smallNote, left: '70%', top: '15%' },
+    { src: smallNote, left: '10%', top: '70%' },
+    { src: smallNote, left: '2%', top: '60%' },
+    { src: smallNote, left: '25%', top: '90%' },
+    { src: smallNote, left: '60%', top: '65%' },
+    { src: smallNote, left: '50%', top: '30%' },
+    { src: smallNote, left: '2%', top: '40%' },
+  ];
+  return (
+    <div className="relative h-full w-full">
+      {/* {coordinates.map((circle, index) => (
+        <Circle
+          key={index}
+          size={circle.size}
+          left={circle.left}
+          top={circle.top}
+          right={circle.right}
+          backgroundColor={circle.backgroundColor}
+        />
+      ))} */}
+      {smallnote.map((note, index) => (
+        <img
+          key={index}
+          src={note.src}
+          alt="음표"
+          style={{
+            position: 'absolute',
+            left: note.left,
+            top: `calc(${note.top} + ${scrollY}px)`,
+          }}
+        />
+      ))}
+      {notes.map((note, index) => (
+        <img
+          key={index}
+          src={note.src}
+          alt="음표"
+          className="animate-float"
+          style={{
+            position: 'absolute',
+            left: note.left,
+            top: `calc(${note.top} + ${scrollY}px)`,
+          }}
+        />
+      ))}
+
+      <div className="relative z-10 flex h-full w-full items-center justify-center">
+        {children}
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, ReactNode } from 'react';
 import './MainPage.scss';
-
-import Background from '../components/Background';
+import { useSpring, animated } from 'react-spring';
+import Background3 from '../components/Background3';
 import { Link } from 'react-router-dom';
 
 interface MainPageProps {
@@ -17,10 +17,12 @@ function MainPage({ children }: MainPageProps) {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const opacity = Math.max(0, Math.min(1, (offsetY - 500) / 500));
+
   return (
     <div className="MainPage">
       <div className="flex h-screen w-screen flex-col items-center justify-center bg-cover">
-        <Background>
+        <Background3>
           <div
             className="MainPage__background"
             style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
@@ -37,25 +39,93 @@ function MainPage({ children }: MainPageProps) {
             </div>
             <div className="MainPage__content__text">
               <div className="MainPage__title">
-                HOW ARE YOU <br /> FEELING TODAY?
+                Brandify <br /> your one-stop solution for custom music, logos,
+                and posters.
               </div>
-              <div>오늘 당신의 기분과 음악을 알려드릴게요!</div>
-              <div className="mt-8 flex w-full justify-between px-4">
+              <div>
+                프로모션 제품을 위한 맞춤형 음악, 로고, 포스터를 제공하는 원스톱
+                솔루션입니다!
+              </div>
+              <div
+                className="mt-8 flex w-full justify-center px-4"
+                style={{ marginTop: '3rem' }}
+              >
+                <Link to="/signup" style={{ marginBottom: '1rem' }}>
+                  <button className="font-['Cafe24 Danjunghae'] hover:border-blue h-[3rem] w-[15rem] items-center justify-center rounded-[2.5rem] border-2 border-white bg-white/30 text-center text-2xl font-normal text-white hover:bg-black hover:text-white">
+                    회원가입
+                  </button>
+                </Link>
                 <Link to="/signin">
                   <button className="font-['Cafe24 Danjunghae'] hover:border-blue h-[3rem] w-[15rem] items-center justify-center rounded-[2.5rem] border-2 border-white bg-white/30 text-center text-2xl font-normal text-white hover:bg-black hover:text-white">
                     로그인
                   </button>
                 </Link>
-                <Link to="/signup">
-                  <button className="font-['Cafe24 Danjunghae'] hover:border-blue h-[3rem] w-[15rem] items-center justify-center rounded-[2.5rem] border-2 border-white bg-white/30 text-center text-2xl font-normal text-white hover:bg-black hover:text-white">
-                    회원가입
-                  </button>
-                </Link>
               </div>
+
+              <div
+                className="w-full justify-between text-center text-5xl text-white"
+                style={{ marginTop: '15rem' }}
+              >
+                저희 사이트의 사용방법을 알려드릴게요
+              </div>
+
+              <div
+                className="w-full justify-between text-center text-4xl text-white"
+                style={{ marginTop: '5rem' }}
+              >
+                step1
+              </div>
+              <div
+                className="w-full justify-between text-center text-4xl text-white"
+                style={{ marginTop: '3rem', opacity: opacity }}
+              >
+                프로모션 하고 싶으신 제품 사진을 찍고 사진을 업로드 한 뒤
+                "브랜딩 start" 버튼을 눌러주세요
+              </div>
+              <div
+                className="w-full justify-between text-center text-4xl text-white"
+                style={{ marginTop: '3rem' }}
+              >
+                step2
+              </div>
+              <div
+                className="w-full justify-between text-center text-4xl text-white"
+                style={{ marginTop: '3rem', opacity: opacity }}
+              >
+                로고와 포스터에 들어갈 택스트와 색상,스타일을 선택하고 생성
+                버튼을 눌러 주세요!
+              </div>
+              <div
+                className="w-full justify-between text-center text-4xl text-white"
+                style={{ marginTop: '3rem' }}
+              >
+                step3
+              </div>
+              <div
+                className="w-full justify-between text-center text-4xl text-white"
+                style={{ marginTop: '3rem', opacity: opacity }}
+              >
+                생성이 완료되었다면 목록에서 이미지를 클릭하세요 생성하신
+                결과물을 다시 보실 수 있답니다.
+              </div>
+
+              <div
+                className="w-full justify-between text-center text-3xl text-white hover:text-blue-500"
+                style={{ marginTop: '5rem' }}
+              >
+                <a
+                  href="https://file.notion.so/f/f/f4c1918f-224c-424d-ae68-fe3cf854f100/b901661b-be63-4092-9298-352465e58421/%E1%84%8C%E1%85%A6%E1%84%86%E1%85%A9%E1%86%A8_%E1%84%8B%E1%85%A5%E1%86%B9%E1%84%8B%E1%85%B3%E1%86%B7_(2)_(1).mp4?id=640d7281-a3b1-46c0-bbe3-60bfc78e2625&table=block&spaceId=f4c1918f-224c-424d-ae68-fe3cf854f100&expirationTimestamp=1722340800000&signature=46SVcYb26feb9nCWRoRvslREcqk8-B0CNaqVZl6nPq4&downloadName=%E1%84%8C%E1%85%A6%E1%84%86%E1%85%A9%E1%86%A8+%E1%84%8B%E1%85%A5%E1%86%B9%E1%84%8B%E1%85%B3%E1%86%B7+%282%29+%281%29.mp4"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  더 자세한 내용을 원하신다면 클릭해 주세요
+                </a>
+              </div>
+
               {children}
             </div>
           </div>
-        </Background>
+        </Background3>
       </div>
     </div>
   );
